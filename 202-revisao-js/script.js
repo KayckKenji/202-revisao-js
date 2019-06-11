9691886d914098bb4474a988c8cb1c5f4159a1c8
Kenji <51339721+KayckKenji@users.noreply.github.com>
2019-06-11 16:57:18 -0300

    revisão sobre javascript

---
+++ 202-revisao-js/script.js
@@ -0,0 +1,10 @@
+    let signos = ["aries", "aquario", "cancer", "peixes", "sagitario", "escorpiao", "libra", "gemeos"];
+    // FARINHA
+    for(let indice in signos){
+        console.log("O item na posição " + indice + " é o " + signos[indice]);
+    }
+    console.log("---------------------------")
+    // FAROFA
+        for(let valores of signos){
+        console.log(valores);
+    }
